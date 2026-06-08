9d51127ec5f6b844d24f9d14379beaf22e7a2dcc
messiapple52 <messiapple52@gmail.com>
2026-06-08 18:24:12 +1000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 const SUPABASE_ANON_KEY =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueHB2eWtlenVpZ2Roa212ZHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjMxMzYsImV4cCI6MjA5MzQzOTEzNn0.hVluf7TVg719lkrLbRpgeenv698dDZTzaIH7o4hQt64";
 
-const supabase =
+const db =
 window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
