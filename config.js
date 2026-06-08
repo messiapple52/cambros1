809810647994338d2e97e546951a7d76fc334d63
messiapple52 <messiapple52@gmail.com>
2026-06-08 18:55:02 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-const SUPABASE_URL =
-"https://xnxpvykezuigdhkmvdvk.supabase.co";
-
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueHB2eWtlenVpZ2Roa212ZHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjMxMzYsImV4cCI6MjA5MzQzOTEzNn0.hVluf7TVg719lkrLbRpgeenv698dDZTzaIH7o4hQt64";
-
-const db =
-window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+window.db = window.supabase.createClient(
+    "https://xnxpvykezuigdhkmvdvk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhueHB2eWtlenVpZ2Roa212ZHZrIiwicm9sZSI6ImFub24iLCJ9.hVluf7TVg719lkrLbRpgeenv698dDZTzaIH7o4hQt64"
 );
